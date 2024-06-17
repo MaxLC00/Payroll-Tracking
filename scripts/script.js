441b266b7476e670b801c3fd818f547be5b550f4
MaxLC00 <167907843+MaxLC00@users.noreply.github.com>
2024-06-17 14:14:13 -0500

    added random num generator

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,8 @@ const displayAverageSalary = function (employeesArray) {
 
 // Select a random employee
 const getRandomEmployee = function (employeesArray) {
-  // TODO: Select and display a random employee
+const randomNum = Math.floor(Math.random()*employeesArray.length)
+console.log(randomNum)
 }
 
 /*
